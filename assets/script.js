0d432aaa14a8b2e605b7fc29df6c0b120970fb98
Anna Battistals <anna.r.battista@gmail.com>
2020-09-28 15:01:14 -0600

    added javascript and style sheet.  Added basic html outline and pseudo-code

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+// GIVEN I am taking a code quiz
+
+// WHEN I click the start button
+    // THEN a timer starts 
+        // function for a count down
+    // THEN I am presented with a question
+        // need a function to hold this
+
+// WHEN I answer a question correctly   
+    // THEN I am presented with another question
+        // need a function to hold this
+
+// WHEN I answer a question incorrectly
+    // THEN time is subtracted from the clock
+        // need a function to remove time from countdown
+
+// WHEN all questions are answered or the timer reaches 0
+    // THEN the game is over
+        // need a way to stop quiz at the end of countdown
+        // give back the score
+
+// WHEN the game is over
+    // THEN I can save my initials and score
+        // prompt user to add initials
+        // create grid on page to hold name and scores
+
+//-------------Variables Needed-------------
+
+
+// --------------Functions Needed-------------
